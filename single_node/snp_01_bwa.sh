f44bf49aa2e07e5a1bc5bdd775d53c576537dc58
mcapuccini <marco.capuccini@it.uu.se>
2020-02-11 11:21:09 +0100

    add new notebooks

--- single_node/snp_01_bwa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Assume input in the same directory
-docker run -d -v $PWD:/host_pwd --name bwa \
-  mcapuccini/alignment:latest \
-  sh -c 'bwa mem -t 16 -p /ref/human_g1k_v37.fasta /host_pwd/HG02666_1_7.fastq | samtools view > /host_pwd/aln.sam'
